873755306c7c238a643ab89efd27fe56e78703ef
pranitchuri <73994212+pranitchuri@users.noreply.github.com>
2025-03-14 13:24:47 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header">
       <h1>Welcome to the DevOps Space!</h1>
-      <p>Frontend Developer & Design Enthusiast</p>
+      <p> Lets Automate things and save Time! </p>
     </header>
   );
 }
