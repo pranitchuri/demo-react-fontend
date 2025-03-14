030e97ea4e77aedb57bfab17e644ec20ddffa9c6
pranitchuri <73994212+pranitchuri@users.noreply.github.com>
2025-03-14 19:26:28 +0530

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,8 @@ function AboutMe() {
   return (
     <section className="about-me">
       <h2>About Me</h2>
-      <p>Hi! I'm Pranit, a passionate DevOps Engineer who loves creating beautiful and functional web experiences.</p>
+      <p>Hi! I'm Pranit, a passionate DevOps Engineer who loves building scalable, reliable, and efficient infrastructure to support seamless application delivery and deployment.
+    </p>
     </section>
   );
 }
